b7d67f4887514bc76dc11156d7688597754e38d7
Giovanni <giovanni_neves@tutanota.com>
2023-10-15 12:37:01 -0300

    Adicionando busca de contas

--- backend/src/main/java/br/dev/giovanni/wayon/account/AccountController.java
+++ backend/src/main/java/br/dev/giovanni/wayon/account/AccountController.java
@@ -1,8 +1,22 @@
 package br.dev.giovanni.wayon.account;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+
+	private final AccountRepository repository;
+	
+	public AccountController(AccountRepository repository) {
+		this.repository = repository;
+	}
+	
+	@GetMapping("/accounts")
+	List<Account> findAll() {
+		return repository.findAll();
+	}
 
 }
