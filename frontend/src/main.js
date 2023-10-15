8f91b2824b4dc88a86ee1468b0c9f5c8f3efedbd
Giovanni <giovanni_neves@tutanota.com>
2023-10-15 18:24:11 -0300

    Terminando a limpesa Instalando Bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,6 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import 'bootstrap/dist/js/bootstrap'
 
 import { createApp } from 'vue'
 import App from './App.vue'
