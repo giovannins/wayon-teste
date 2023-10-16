0a1e8c62d328653ba97bde1e7d4b22782e73671f
Giovanni Neves Sadauscas <giovanni_neves@tutanota.com>
2023-10-16 09:03:54 -0300

    Melhorando navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,12 +5,24 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
 
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/extrato">Extrato</RouterLink>
-      </nav>
-    </div>
+    <nav class="navbar bg-dark navbar-expand-lg" data-bs-theme="dark">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <RouterLink class="nav-link" active-class="active" to="/">Home</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" active-class="active" to="/extrato">Extrato</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
 
   <RouterView />
